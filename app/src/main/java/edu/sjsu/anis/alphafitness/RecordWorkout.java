4fb97ed95b02890c6afd6df49257dbc2745855a6
Anis Dhapa <anisdhapa11@gmail.com>
2018-03-13 13:03:41 -0700

    commit everything

---
+++ app/src/main/java/edu/sjsu/anis/alphafitness/RecordWorkout.java
@@ -0,0 +1,13 @@
+package edu.sjsu.anis.alphafitness;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecordWorkout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_workout);
+    }
+}
