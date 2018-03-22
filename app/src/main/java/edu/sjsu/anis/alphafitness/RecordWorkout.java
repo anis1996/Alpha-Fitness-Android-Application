8ea85e01fe5b5970cfe1e187d044ba658d17a22e
Anis Dhapa <anisdhapa11@gmail.com>
2018-03-22 10:08:56 -0700

    Workout Fragment is running without Google map

--- app/src/main/java/edu/sjsu/anis/alphafitness/RecordWorkout.java
+++ app/src/main/java/edu/sjsu/anis/alphafitness/RecordWorkout.java
@@ -1,5 +1,9 @@
 package edu.sjsu.anis.alphafitness;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
+
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +13,22 @@ public class RecordWorkout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_workout);
+
+        FragmentManager fm = getSupportFragmentManager();
+
+
+        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+
+
+        } else {
+
+            Fragment fragment = fm.findFragmentById(R.id.fragmentcontainer);
+                fragment = new WorkoutFragment();
+                fm.beginTransaction()
+                        .replace(R.id.fragmentcontainer, fragment)
+                        .commit();
+
+        }
+
     }
 }
